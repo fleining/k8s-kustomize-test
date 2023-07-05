1faeea7dd5c6f80589c8c3c0095a3d46442a7937
el6max <fleinmax@gmail.com>
2023-07-05 17:26:49 +0800

    upd realworld

--- realworld/kustomize_notes.sh
+++ realworld/kustomize_notes.sh
@@ -23,3 +23,25 @@ diff -u -N /tmp/LIVE-042965687/autoscaling.v1.HorizontalPodAutoscaler.kustom.fro
      apiVersion: apps/v1
 
 
+kustomize build overlays/dev
+kubectl diff -k overlay/dev
+oc diff -k overlays/dev
+
+##############################################3
+
+Kustomize Best Practices
+- Keep your custom resources and their instances in separate packages, otherwise you will encounter race conditions and your creation will get stuck. For example, many people keep both the CertManager CRD and CertManager’s resources in the same package, which can cause problems. Most of the time, reapplying the YAML fixes the issue. But it's good practice to keep them separately.
+- Try to keep the common values like namespace, common metadata in the base file.
+- Organize your resources by kind, using the following naming convention: lowercase-hypenated.yaml (e.g., horizontal-pod-autoscaler.yaml). Place services in the service.yaml file.
+- Follow standard directory structure, using bases/ for base files and patches/ or overlays/ for environment-specific files.
+- While developing or before pushing to git, run kubectl kustomize cfg fmt file_name to format the file and set the indentation right.
+
+crd/
+  cert_crd.yaml
+base_resources/
+  cert-manager.yaml
+common/
+  namespace.yaml
+  metadata.yaml
+  service.yaml
+patches/
